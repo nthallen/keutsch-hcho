433dce84ae40d091283d99036d3a26c99ca9bf25
Norton Allen <allen@huarp.harvard.edu>
2017-11-23 21:29:55 -0500

    Change type of PPS_drift from secs to msecs

--- eng/ghchopps.m
+++ eng/ghchopps.m
@@ -1,5 +1,5 @@
 function fig = ghchopps(varargin);
 % ghchopps(...)
 % PPS
-ffig = ne_group(varargin,'PPS','phchoppst','phchoppsr','phchoppsf');
+ffig = ne_group(varargin,'PPS','phchoppsd','phchoppsr','phchoppsf');
 if nargout > 0 fig = ffig; end
