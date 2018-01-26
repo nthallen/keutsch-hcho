ec58a8adaf8bae39e5f0c4bfc526bd2b4f3b9285
Norton Allen <allen@huarp.harvard.edu>
2018-01-26 15:30:02 -0500

    Initial changes adding:   Telemetry frame down from 20 Hz to 10 Hz after update to TMC   BCtr updates to retrieve higher rate laser power as well as     scan and chop status   Updated algorithm to support multiple inter-scan periods and     online dithering

--- TM/dacscan.cmd
+++ TM/dacscan.cmd
@@ -8,12 +8,24 @@
       uint16_t usp;
       double sp = floor($4*65536/5000);
       if ($3 == 0x84) sp = sp*8; // The Step has *8 resolution
-      if (sp < 0) sp = 0;
-      else if (sp > 65535) sp = 65535;
-      usp = floor(sp);
+      if ($3 == 0x86) { // offline delta is signed
+        int16_t ssp;
+        if (sp < -32768.) ssp = -32768;
+        else if (sp > 32767) ssp = 32767;
+        else ssp = floor(sp);
+        usp = (uint16_t)ssp;
+      } else {
+        if (sp < 0) sp = 0;
+        else if (sp > 65535) sp = 65535;
+        usp = floor(sp);
+      }
 
       if (SB.BCtr)
         SB.BCtr->sbwr($3, usp);
+    }
+  : Laser Chop NPts &NPtsOnOff %d * {
+      if (SB.BCtr)
+        SB.BCtr->sbwr($4, $5);
     }
   : Laser Command &lasercmd * {
         if (SB.BCtr)
@@ -26,17 +38,23 @@
   : Scan Start { $0 = 0x82; }
   : Scan Stop { $0 = 0x83; }
   : Scan Step { $0 = 0x84; }
-  : Online { $0 = 0x85; }
-  : Offline { $0 = 0x86; }
-  : Dither { $0 = 0x87; }
+  : Online Position { $0 = 0x85; }
+  : Offline Delta { $0 = 0x86; }
+  : Online Dither { $0 = 0x87; }
   ;
+
+&NPtsOnOff <uint16_t>
+  : Online { $0 = 0x88; }
+  : Offline { $0 = 0x89; }
 
 &lasercmd <uint16_t>
   : Scan Start { $0 = 1; }
   : Scan Abort { $0 = 0; }
   : Drive Online { $0 = 2; }
-  : Drive Dither Above Online { $0 = 3; }
-  : Drive Dither Below Online { $0 = 4; }
+  : Dither Online Up { $0 = 3; }
+  : Dither Online Down { $0 = 4; }
   : Drive Offline { $0 = 5; }
+  : Chop Mode Begin { $0 = 6; }
+  : Chop Mode End { $0 = 7; }
   ;
 
