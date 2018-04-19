8be37a606ed890af5e1ef20c0bbb489f99cfbfbe
Joshua Shutter <jdshutter@outlook.com>
2018-04-18 21:31:10 -0400

    Moving scripts from master to v1.5.0
    Moving scripts from master branch to v1.5.0 branch

--- eng/workup/scancorrect.m
+++
@@ -1,9 +0,0 @@
-% Fix scan index issue (FPGA switches to online label before the scan is
-% actually complete)
-
-function NewIndices  = scancorrect(i)
-chunks = chunker(i);
-NewChunks = [chunks(:,1) chunks(:,2)+25]; 
-%There are 15 points at the end not labeled as scan points
-NewIndices = ChunkIndices(NewChunks);
-
