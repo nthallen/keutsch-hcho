efb82bf1f610ab2c0c2e484c246829e4c19caaac
Norton Allen <allen@huarp.harvard.edu>
2018-09-07 09:28:21 -0600

    Plotting of normalized counts. Mixing ratio calc work

---
+++ TM/HCHO_CAL.h
@@ -0,0 +1,12 @@
+/* HCHO_CAL_CONSTANT units are
+    counts/sec/mW/ppb of HCHO
+
+  2018-09-05 JDS 71.87
+  2018-09-07 NTA 2700 based on back-of-the-envelope
+
+  The laser power calibration from Volts to mW
+  is defined in TS.tmc. Record changes here.
+
+  2018-09-07 NTA 3.91 V == 1 mW
+*/
+#define HCHO_CAL_CONSTANT 2700.
