c49bd699a31faf148c5bd1ffdee3cc0306363f83
Norton Allen <allen@huarp.harvard.edu>
2018-09-07 15:33:32 -0400

    Rerecalibration for mixing ratio

--- TM/HCHO_CAL.h
+++ TM/HCHO_CAL.h
@@ -3,10 +3,12 @@
 
   2018-09-05 JDS 71.87
   2018-09-07 NTA 2700 based on back-of-the-envelope
+  2018-09-07 NTA/JDS 71.87 reverting to Josh's number after power cal
 
   The laser power calibration from Volts to mW
   is defined in TS.tmc. Record changes here.
 
   2018-09-07 NTA 3.91 V == 1 mW
+  2018-09-07 NTA/JDS nonlinear cal
 */
-#define HCHO_CAL_CONSTANT 2700.
+#define HCHO_CAL_CONSTANT 71.87
