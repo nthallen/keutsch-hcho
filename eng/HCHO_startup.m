2bfe46c3552bd08504085a7e10148af9e1e01446
Norton Allen <allen@huarp.harvard.edu>
2024-01-16 10:20:54 -0500

    getrun config

--- eng/HCHO_startup.m
+++ eng/HCHO_startup.m
@@ -1,3 +1,14 @@
+function HCHO_startup(ui_func_handle, data_dir_func_name, title)
+% getrun_startup(ui_func_handle, data_dir_func_name, title);
+% ui_func_handle: handle of the ui_ function to start the instrument's
+%   engineering graphing tool
+% data_dir_func_name: char array with the name of the data_dir_func.
+% title: (optional) char array specifying the title to display on the UI
+%
+% The directory reported by the data_dir_func is the location where
+% runs.dat is located.
+pdir = ne_load_runsdir(data_dir_func_name);
+cd(pdir);
 [fd,~] = fopen('runs.dat','r');
 if fd > 0
     tline = fgetl(fd);
@@ -38,4 +49,8 @@ if fd > 0
     delete runs.dat
 end
 clear fd msg oldfolder tline
-ui_hcho;
+if nargin < 3
+  ui_func_handle(data_dir_func_name);
+else
+  ui_func_handle(data_dir_func_name, title);
+end
